ec14ce787752801c8dd0fce4ff4b532cd32b6dc4
Sardorbek-Raximberdiyev <sardorraximberdiyev9@gmail.com>
2022-08-14 22:08:13 +0500

    Add flexibility feature to header section.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const elModal = document.querySelector('.modal');
 const elCloseButton = document.querySelector('.js-close-button');
+const elSiteHeader = document.querySelector('.sayt-header');
+const elOpenHeaderButton = elSiteHeader.querySelector('.js-open-button');
+
+
+if (elOpenHeaderButton) {
+  elOpenHeaderButton.addEventListener('click', function () {
+    elSiteHeader.classList.toggle('site-header-open');
+  })
+}
 
 if (elCloseButton) {
 elCloseButton.addEventListener('click', function () {
